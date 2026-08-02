fb914d34cf9f41ff9f63fb8ab65c68c550fe0fd9
Tobias Buckdahn <info@brickup.de>
2026-08-02 12:42:43 +0200

    Refactor program

--- sketch_v6_arcade_buttons.ino
+++ sketch_v6_arcade_buttons.ino
@@ -3,120 +3,193 @@
 #include <SoftwareSerial.h>
 #include <TM1637Display.h>
 
-#define BTN_INCREASE 2 // green button
-#define BTN_DECREASE 5 // red button
-#define LED_INCREASE 3 // green LED
-#define LED_DECREASE 4 // red LED
-#define DISPLAY_CLK A5 // 4x7-segment-display
+// ########################################## //
+//                   WIRING                   //
+//       arduino port of each component       //
+// ########################################## //
+#define BUTTON_INCREASE 2
+#define BUTTON_INCREASE_LED 3
+#define BUTTON_DECREASE 5
+#define BUTTON_DECREASE_LED 4
+#define DISPLAY_CLK A5
 #define DISPLAY_DIO A4
-#define TONE_BUZZER 6
+#define SOUND_BUZZER 6
 #define RS485_DI 8
 #define RS485_DE 9
 #define RS485_RE 10
 #define RS485_RO 11
 
-#define BTN_DEBOUNCE_MS 0
-#define LED_FLASH_MS 200
-#define LINK_INTERVAL_MS 2000
-#define LINK_INTERVAL_JITTER_MS 400
-#define LINK_TIMEOUT_MS 10000
-#define LINK_BLINK_MS 500
-#define ALARM_TONE_HZ 2000
-#define ALARM_TONE_MS 100
-#define ALARM_TONE_GAP_MS 150
-#define ALARM_REPEAT_MS 5000
+// ########################################## //
+//                   CONFIG                   //
+//  adjustable values inside of the program   //
+// ########################################## //
+#define BUTTON_DEBOUNCE_MS 20
+#define CONNECTION_BAUD_RATE 9600
+#define SERIAL_BAUD_RATE 9600
 
-AcksenButton btnIncrease(BTN_INCREASE, ACKSEN_BUTTON_MODE_NORMAL, BTN_DEBOUNCE_MS, INPUT_PULLUP);
-AcksenButton btnDecrease(BTN_DECREASE, ACKSEN_BUTTON_MODE_NORMAL, BTN_DEBOUNCE_MS, INPUT_PULLUP);
-TM1637Display disTotal(DISPLAY_CLK, DISPLAY_DIO);
-SoftwareSerial rs485(RS485_RO, RS485_DI);
-
-int32_t local = 0;
-int32_t remote = 0;
-int32_t total = 0;
-int32_t synci = 0;
-
-bool ledIncreaseFlash = false;
-bool ledDecreaseFlash = false;
-unsigned long ledIncreaseFlashStart = 0;
-unsigned long ledDecreaseFlashStart = 0;
-
-unsigned long lastSendMs = 0;
-unsigned long sendIntervalMs = LINK_INTERVAL_MS;
-unsigned long lastRemoteMs = 0;
-bool remoteSeen = false;
-
-unsigned long lastAlarmMs = 0;
-bool secondChirpPending = false;
-
+// ########################################## //
+//                SETUP & LOOP                //
+//  adjustable values inside of the program   //
+// ########################################## //
 void setup() {
-  disTotal.setBrightness(4);
-  disTotal.showNumberDec(total, true);
+  Serial.begin(SERIAL_BAUD_RATE);
 
-  Serial.begin(9600);
-  rs485.begin(9600);
-
-  pinMode(RS485_DE, OUTPUT);
-  pinMode(RS485_RE, OUTPUT);
-  digitalWrite(RS485_DE, LOW);
-  digitalWrite(RS485_RE, LOW);
-
-  pinMode(LED_INCREASE, OUTPUT);
-  pinMode(LED_DECREASE, OUTPUT);
-  digitalWrite(LED_INCREASE, LOW);
-  digitalWrite(LED_DECREASE, LOW);
-
-  pinMode(TONE_BUZZER, OUTPUT);
-
-  randomSeed(analogRead(A0));
+  setupButtons();
+  setupConnection();
+  setupDisplay();
+  setupSound();
 }
 
 void loop() {
+  loopButtons();
+  loopConnection();
+  loopDisplay();
+  loopSound();
+}
+
+// ########################################## //
+//                   COUNT                    //
+// ########################################## //
+int32_t localCount = 0;
+int32_t remoteCount = 0;
+int32_t totalCount() {
+  return localCount + remoteCount;
+}
+
+void changeLocalCount(int8_t delta) {
+  setLocalCount(localCount + delta);
+}
+
+void setLocalCount(int32_t local) {
+  if (localCount == local)
+    return;
+
+  localCount = local;
+  Serial.print("setLocalCount: ");
+  Serial.println(localCount);
+}
+
+void setRemoteCount(int32_t remote) {
+  if (remoteCount == remote)
+    return;
+
+  remoteCount = remote;
+  Serial.print("setRemoteCount: ");
+  Serial.println(remoteCount);
+}
+
+// ########################################## //
+//                  BUTTONS                   //
+//      Arcade switches with an LED each      //
+// ########################################## //
+AcksenButton btnIncrease(BUTTON_INCREASE, ACKSEN_BUTTON_MODE_NORMAL, BUTTON_DEBOUNCE_MS, INPUT_PULLUP);
+AcksenButton btnDecrease(BUTTON_DECREASE, ACKSEN_BUTTON_MODE_NORMAL, BUTTON_DEBOUNCE_MS, INPUT_PULLUP);
+
+void setupButtons() {
+  pinMode(BUTTON_INCREASE_LED, OUTPUT);
+  pinMode(BUTTON_DECREASE_LED, OUTPUT);
+  digitalWrite(BUTTON_INCREASE_LED, LOW);
+  digitalWrite(BUTTON_DECREASE_LED, LOW);
+}
+
+void loopButtons() {
   btnIncrease.refreshStatus();
   btnDecrease.refreshStatus();
 
-  if (btnIncrease.onPressed()) {
-    local++;
-    Serial.print("local/increase: ");
-    Serial.println(local);
-    refresh();
-  }
+  if (btnIncrease.onPressed())
+    changeLocalCount(+1);
+  if (btnDecrease.onPressed())
+    changeLocalCount(-1);
+}
 
-  if (btnDecrease.onPressed()) {
-    local--;
-    Serial.print("local/decrease: ");
-    Serial.println(local);
-    refresh();
-  }
+// ########################################## //
+//                  DISPLAY                   //
+//         4-digit 7-segment-display          //
+// ########################################## //
+TM1637Display display(DISPLAY_CLK, DISPLAY_DIO);
 
-  if (rs485.available() > 0) {
-    remote = rs485.parseInt();
-    rs485.read(); // -> "|"
-    Serial.print("remote/receive: ");
-    Serial.println(remote);
-    refresh();
-  } else if (synci++ > 10000) {
-    synci = 0;
-    sendLocal();
+void setupDisplay() {
+  display.setBrightness(1);
+  displayNumber(0);
+}
+
+void loopDisplay() {
+  int32_t count = totalCount();
+  displayNumber(count);
+}
+
+void displayNumber(int32_t number) {
+  static int32_t displayed = 8888;
+  if (displayed != number) {
+    display.showNumberDec(number, false);
+    displayed = number;
   }
 }
 
-void sendLocal() {
-  digitalWrite(RS485_DE, HIGH);
-  digitalWrite(RS485_RE, HIGH);
+// ########################################## //
+//                 CONNECTION                 //
+//            between the counters            //
+// ########################################## //
+SoftwareSerial connection(RS485_RO, RS485_DI);
 
-  rs485.print(local);
-  rs485.print("|");
-  rs485.flush();
+void setupConnection() {
+  pinMode(RS485_DE, OUTPUT);
+  pinMode(RS485_RE, OUTPUT);
+  setSending(false);
 
-  digitalWrite(RS485_DE, LOW);
-  digitalWrite(RS485_RE, LOW);
-
-  Serial.print("local/send: ");
-  Serial.println(local);
+  connection.begin(9600);
 }
 
-void refresh() {
-  total = abs(local - remote);
-  disTotal.showNumberDec(total, false);
+void setSending(bool transmit) {
+  digitalWrite(RS485_DE, transmit ? HIGH : LOW);
+  digitalWrite(RS485_RE, transmit ? HIGH : LOW);
+}
+
+void loopConnection() {
+  if (connection.available() > 0) {
+    receiveRemoteCount();
+  } else {
+    sendLocalCountFrequently();
+  }
+}
+
+void sendLocalCountFrequently() {
+  static int32_t iteration = 0;
+
+  if (iteration++ > 10000) {
+    sendLocalCount();
+    iteration = 0;
+  }
+}
+
+void sendLocalCount() {
+  setSending(true);
+  connection.print(localCount);
+  connection.print("|");
+  connection.flush();
+  setSending(false);
+
+  Serial.print("sendLocalCount: ");
+  Serial.println(localCount);
+}
+
+void receiveRemoteCount() {
+  int32_t remote = connection.parseInt();
+  char separator = connection.read();
+  setRemoteCount(remote);
+
+  Serial.print("receiveRemoteCount: ");
+  Serial.println(localCount);
+}
+
+// ########################################## //
+//                    SOUND                   //
+//   passive buzzer to produce beep sounds    //
+// ########################################## //
+
+void setupSound() {
+  pinMode(SOUND_BUZZER, OUTPUT);
+}
+
+void loopSound() {
 }
